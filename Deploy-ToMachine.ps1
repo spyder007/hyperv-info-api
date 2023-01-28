69f0c7774fb9755119e933691209729c8e569cf9
Matt Gerega <gerega@gmail.com>
2023-01-28 15:50:00 -0500

    Fixed remote execution

--- Deploy-ToMachine.ps1
+++ Deploy-ToMachine.ps1
@@ -12,11 +12,11 @@ Pop-Location
 
 $s = New-PSSession -ComputerName $machineName
 Enter-PSSession -Session $s
-Invoke-Command -Session $s -ScriptBlock { Write-Host "Stopping Service"; Stop-Service "$serviceName" }
+Invoke-Command -Session $s -ArgumentList $serviceName -ScriptBlock { Write-Host "Stopping Service $($args[0])"; Stop-Service "$($args[0])" }
 Write-Host "Copying files to $machineName"
 Copy-Item -Recurse -Force spydersoft.hyperv.info\bin\Release\net6.0\* "\\$($machineName)\$($serviceSharePath)"
 
 
-Invoke-Command -Session $s -ScriptBlock { Write-Host "Starting Service"; Start-Service "$serviceName" }
+Invoke-Command -Session $s -ArgumentList $serviceName -ScriptBlock { Write-Host "Starting Service $($args[0])"; Start-Service "$($args[0])" }
 Remove-PSSession -Session $s
 
