e4b1ef437ebec1cd6404a9ebabed73cb734c134c
Matt Gerega <gerega@gmail.com>
2023-12-05 20:54:40 -0500

    Updated deployment script to save server app settings

--- Deploy-ToMachine.ps1
+++ Deploy-ToMachine.ps1
@@ -13,9 +13,15 @@ Pop-Location
 $s = New-PSSession -ComputerName $machineName
 Enter-PSSession -Session $s
 Invoke-Command -Session $s -ArgumentList $serviceName -ScriptBlock { Write-Host "Stopping Service $($args[0])"; Stop-Service "$($args[0])" }
+
+Write-Host "Saving Appsettings";
+Copy-Item "\\$($machineName)\$($serviceSharePath)\appsettings.json" "temp.appsettings.json"
+
 Write-Host "Copying files to $machineName"
 Copy-Item -Recurse -Force Spydersoft.Hyperv.Info\bin\Release\net6.0\* "\\$($machineName)\$($serviceSharePath)"
 
+Write-Host "Loading Appsettings";
+Move-Item "temp.appsettings.json" "\\$($machineName)\$($serviceSharePath)\appsettings.json" -Force
 
 Invoke-Command -Session $s -ArgumentList $serviceName -ScriptBlock { Write-Host "Starting Service $($args[0])"; Start-Service "$($args[0])" }
 Remove-PSSession -Session $s
